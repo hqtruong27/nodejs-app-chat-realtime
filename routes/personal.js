2ea170731e914af106e160b0cf09df59f2c1fab4
Truong Hoang <hqtruong27@gmail.com>
2021-09-07 16:47:00 +0700

    no change

--- routes/personal.js
+++ routes/personal.js
@@ -4,9 +4,7 @@ const router = express.Router()
 const middleware = require('../middleware/middleware')
 
 router.get('/', middleware.verify, (req, res, next) => {
-    const user = req.user
-    console.log('', user)
-    return res.json(user)
+    return res.json(req.user)
 })
 
 module.exports = router
