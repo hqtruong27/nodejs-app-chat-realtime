80345e2cccf96c5041a78f92f14d3fcb2fb7c1fb
Truong Hoang <hqtruong27@gmail.com>
2021-09-08 21:21:18 +0700

    sm change

--- routes/controller.js
+++ routes/controller.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const router = express.Router()
-const { dbConnection } = require('../src/database/mongo')
+const UserGroup = require('../model/userGroup')
+
 /**
  * Home page: loading all product
  */
 router.get('/group?:userId', async (req, res) => {
-    const _context = await dbConnection()
-    const userId = req.query.userId
-    let result = await _context.collection('Groups').find({ userId: userId }).toArray()
+    try {
+        //const newChat = new Chat({ groupId: groupId, message_sender: userId, message: { text: 'hah hah...' } })
+        //const rs = await newChat.save()
+        const result = await Groups.find({})
 
-    console.log(result)
+        console.log(result)
 
-    return res.send(result)
+        return res.send(result)
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({ code: 500, message: error.message })
+    }
 })
 
 router.get('/chat?:groupId', async (req, res) => {
