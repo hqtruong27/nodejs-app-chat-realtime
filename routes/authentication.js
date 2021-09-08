b2f7187b78bc9ae2e0e1d52f08ce67a385d79064
Truong Hoang <hqtruong27@gmail.com>
2021-09-09 02:21:51 +0700

    api checkToken

--- routes/authentication.js
+++ routes/authentication.js
@@ -11,14 +11,17 @@ const middleware = require('../middleware/middleware')
 const rounds = 9
 const SECRET_KEY = process.env.SECRET_KEY || 'my secret key'
 
-router.get('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
         const user = await User.findOne({ name: req.body.name })
         if (user != null) {
             if (await bcrypt.compare(req.body.password, user.password)) {
                 const token = generateToken(user)
 
-                return res.status(200).json({ token: token, expiresIn: new Date(Date.now() + 24 * 3600 * 1000) })
+                return res.status(200).json({
+                    token: token,
+                    expiresIn: new Date(Date.now() + 600000)
+                })
             }
 
             return res.status(401).json({
@@ -27,7 +30,7 @@ router.get('/login', async (req, res) => {
             })
         }
 
-        return res.status(404).json({ code: 404, message: 'user not found' })
+        return res.status(404).json({ code: 404, message: 'This account is not registered' })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ code: 500, message: error.message })
@@ -60,15 +63,10 @@ router.post('/signup', async (req, res) => {
     }
 })
 
-const generateToken = (user) => jwt.sign({ data: user }, SECRET_KEY, { expiresIn: '24h' })
+const generateToken = (user) => jwt.sign({ data: user }, SECRET_KEY, { expiresIn: '10m' })
 
-router.get('/user', middleware.verify, async (req, res, next) => {
-    const users = await User.find()
-    if (users.length > 0) {
-        return res.status(200).json(users)
-    }
-
-    return res.status(404).json({ message: 'User not found' })
+router.get('/checkToken', middleware.verify, async (req, res, next) => {
+    return res.status(200).json()
 })
 
 module.exports = router
