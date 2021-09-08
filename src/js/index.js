2097d25f6cadb80f761507da5200d05ebe05d4ac
Truong Hoang <hqtruong27@gmail.com>
2021-09-08 21:21:34 +0700

    authorize jwt and sm change

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,31 @@
 //import { getHash } from '.././helper/helper.js';
-const socket = io()
 const chatForm = document.querySelector('#chat-form')
 const chatMessage = document.querySelector('.chat-message')
 const sendMessage = document.querySelector('.send-message')
+const typing = document.querySelector('#chat-sending')
 
-const ACCESS_TOKEN = localStorage.getItem('user') || ''
-let User;
-
+const ACCESS_TOKEN = localStorage.getItem('users') || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjYxMzY0MmNiNWRhNTRjMzFkNDczNTBmMSIsIm5hbWUiOiJ0cnVvbmdocSIsInBhc3N3b3JkIjoiJDJiJDA5JFVrdi9EZXdNeEhvRFEwbU51U0U4YWVJL1BtT2ZabEVSNFR2bFczd0RRcVBsa3g2a0VJMWVlIiwiX192IjowfSwiaWF0IjoxNjMxMDM3MjYyLCJleHAiOjE2MzExMjM2NjJ9.GsX_otGeccB_GhEYPOhAZ_cmgurhop4Le1S3zmJlJhE'
 const ChatClient = {
     init: () => {
-        ChatClient.user()
-        ChatClient.socket()
-        ChatClient.event()
+        ChatClient.connect()
+    },
+    connect: () => {
+        let socket = io({ auth: { token: ACCESS_TOKEN } })
+        socket.on('user-info', (res) => {
+            console.log(res)
+            socket.user = res.user
+            socket.emit("join-room", { user: res.user, roomName: "room1" })
+        })
+
+        socket.on("connect_error", (err) => {
+            console.log(err instanceof Error); // true
+            console.log(err.data);
+            console.log(err.message);
+            socket.disconnect()
+        });
+
+        ChatClient.socket(socket)
+        ChatClient.event(socket)
     },
     user: async () => {
         try {
@@ -23,7 +37,7 @@ const ChatClient = {
             })
 
             if (response.ok) {
-                return User = response.data
+                return User = await response.json()
             }
 
             window.location.href = 'https://google.com'
@@ -32,20 +46,21 @@ const ChatClient = {
         }
     },
 
-    socket: () => {
+    socket: (socket) => {
         const chatArea = document.querySelector('.chat-area-main')
-
         socket.on('user-chat', (res) => {
-            console.log(res)
-            const user = res.user
-            const isOwner = user.id == User.id ? 'owner' : ''
-            const image = user?.image?.slice(0, 4) == 'http' ? user?.image : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%2812%29.png'
+            if (socket.id !== res.message_sender) {
+                typing.textContent = ''
+            }
+
+            const isOwner = socket.id === res.message_sender ? 'owner' : ''
+            const image = res.user?.image?.slice(0, 4) == 'http' ? res.user?.image : 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/3364143/download+%2812%29.png'
             const str = `<div class="chat-msg ${isOwner}">
                             <div class="chat-msg-profile">
                                 <img class="chat-msg-img"
                                     src="${image}"
                                     alt="" />
-                                <div class="chat-msg-date">${user.name} - Message seen 2.50pm</div>
+                                <div class="chat-msg-date">${res.user.name} - Message seen 2.50pm</div>
                             </div>
                             <div class="chat-msg-content">
                                 <div class="chat-msg-text">${res.message}</div>
@@ -59,7 +74,7 @@ const ChatClient = {
         })
     },
 
-    event: () => {
+    event: (socket) => {
         chatMessage.addEventListener('keyup', (e) => {
             const IS_ENTER = 13
             const message = e.target.value.trim() ?? ''
@@ -75,20 +90,36 @@ const ChatClient = {
 
         sendMessage.addEventListener('click', (e) => {
             e.preventDefault()
-            if (!User) {
-                ClientChat.user()
-            }
-
             const message = chatMessage.value.trim()
             if (message != '') {
-                socket.emit('on-chat', { user: User, message: message })
+                socket.emit('on-chat', { user: socket.user, message: message })
                 chatMessage.value = ''
             }
         })
+
+        chatMessage.addEventListener('input', () => onInput(socket.user))
+
+        socket.on('typing', (data) => {
+            typing.textContent = data
+        })
+
+        let isTyping = false;
+        let timeout = undefined;
+        const onInput = (user) => {
+            clearTimeout(timeout);
+            socket.emit('typing', `${user.name} is typing...`);
+            timeout = setTimeout(typingTimeout, 3000);
+        }
+        const typingTimeout = () => {
+            isTyping = false;
+            socket.emit('typing');
+        };
+
     },
 
     scrollToBottom: () => {
         chatForm.scrollTop = chatForm.scrollHeight
+        //window.scrollTo(0, document.body.scrollHeight); this worked
     }
 }
 
